8365faf2c622e63b0c6ea1202c9de84e69ca02ab
OmiSeerDev <omarmtinajero@gmail.com>
2023-09-02 23:05:59 -0600

    Adding|

--- NoteBlog/src/main.rs
+++ NoteBlog/src/main.rs
@@ -1,3 +1,33 @@
+#[macro_use]
+extern crate diesel;
+
+use diesel::{Connection, RunQueryDsl};
+use dotenvy::dotenv;
+use std::env;
+use::diesel::pg::PgConnection;
+use::diesel::prelude;
+
+use crate::models::Post;
+
+pub mod schema;
+pub mod models;
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+
+    use self::schema::posts::dsl::*;
+
+    let db_url = env::var("DATABASE_URL").expect("Database url was not found");
+
+    let conn = &mut PgConnection::establish(&db_url).expect("Unable to connect to DB.");
+    
+
+   let post_result = posts.load::<Post>(conn).expect("Wrong query");
+
+   for post in post_result {
+    println!("{}", post.title);
+   }
+    println!("C");
+
+
 }
