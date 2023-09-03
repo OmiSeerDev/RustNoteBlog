fe973e42720e9c1477bc595490cef28ce38ea143
OmiSeerDev <omarmtinajero@gmail.com>
2023-09-02 23:26:41 -0600

    Adding an insert query

--- NoteBlog/src/main.rs
+++ NoteBlog/src/main.rs
@@ -7,7 +7,7 @@ use std::env;
 use::diesel::pg::PgConnection;
 use::diesel::prelude;
 
-use crate::models::Post;
+use crate::models::{Post, NewPost};
 
 pub mod schema;
 pub mod models;
@@ -20,6 +20,14 @@ fn main() {
     let db_url = env::var("DATABASE_URL").expect("Database url was not found");
 
     let conn = &mut PgConnection::establish(&db_url).expect("Unable to connect to DB.");
+
+    let new_post = NewPost {
+        title: "Primer registro",
+        body: "Lorem ipsum...",
+        slug: "primer-post",
+    };
+
+    diesel::insert_into(posts).values(new_post).get_result::<Post>(conn).expect("Fallo al insertar datos");
     
 
    let post_result = posts.load::<Post>(conn).expect("Wrong query");
